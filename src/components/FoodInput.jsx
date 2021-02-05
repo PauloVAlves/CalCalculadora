f606d589de64c5de81dedba8864622e510da1c48
PauloVAlves <paulovalves@outlook.com>
2021-02-05 14:41:38 -0300

    save

--- src/components/FoodInput.jsx
+++ src/components/FoodInput.jsx
@@ -1,13 +1,11 @@
 const FoodInput = () => {
-    return(
-        
+    return(        
             <form>
                 <label htmlFor="addName">Alimento:</label>
                 <input type="text"/>
                 <label htmlFor="addQuantity">Quantidade</label>
                 <input type="number" name="add-quantity" id="add-quantity"/>
             </form>
-        
     )
 }
 
