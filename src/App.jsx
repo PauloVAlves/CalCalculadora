83579d0d9d7b028c0ad331199d87a7294e6d197f
Paulo Alves <paulovalves@outlook.com>
2021-02-09 02:46:04 -0300

    remove unused link

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Nav/Navbar';
 import Form from './components/Form/Form';
 import FoodTable from './components/Table/FoodTable';
