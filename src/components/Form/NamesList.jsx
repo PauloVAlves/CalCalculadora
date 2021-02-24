c5816a728ac2c58890d1c21296384939a9a0ee3a
Paulo Alves <paulovalves@outlook.com>
2021-02-24 17:22:42 -0300

    Change description to name

--- src/components/Form/NamesList.jsx
+++ src/components/Form/NamesList.jsx
@@ -1,12 +1,12 @@
 import { useContext } from 'react';
-import {DataContext} from '../../data/DataContext';
+import { DataContext } from '../../data/DataContext';
 
 const NamesList = () => {
   const { foods } = useContext(DataContext);
   return (
     <datalist className='name-list' id='names-list'>
       {foods.map((food) => (
-        <option key={food.id}>{food.description}</option>
+        <option key={food.id}>{food.name}</option>
       ))}
     </datalist>
   );
