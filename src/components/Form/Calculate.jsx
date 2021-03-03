119427b8959a8b196993802b59d295ce9a5a081c
PauloVAlves <paulovalves@outlook.com>
2021-03-03 12:05:31 -0300

    clear duplicated props

--- src/components/Form/Calculate.jsx
+++ src/components/Form/Calculate.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { DataContext } from '../../data/DataContext';
 import styled from 'styled-components';
 import Button from './Button';
@@ -53,7 +53,6 @@ const Calculate = () => {
           type='number'
           name='calculate'
           placeholder='em gramas'
-          name='calculate'
           id='calculate'
           value={portion}
           onChange={(e) => setPortion(e.target.value)}
@@ -96,35 +95,6 @@ const CalculateForm = styled.form`
     Button {
       width: 80%;
     }
-  }
-`;
-
-const PortionInput = styled.input.attrs((props) => ({
-  type: 'number',
-  placeholder: 'em gramas',
-}))`
-  width: 100%;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 25px;
-  border: 1px solid #ccc;
-  border-radius: 10px;
-  font-size: 1.1rem;
-  text-align: center;
-  padding: 10px;
-  outline: none;
-  transition: all 0.5s ease;
-
-  &:hover {
-    transition: all 0.5s ease;
-    border: 1px solid #000;
-  }
-
-  @media (max-width: 900px) {
-    display: block;
-    margin: auto;
-    margin-bottom: 10px;
-    width: 80%;
   }
 `;
 
