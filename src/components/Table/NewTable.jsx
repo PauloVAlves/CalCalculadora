622f61d54ec7f71da78f4f8dae970bdff1988ad5
PauloVAlves <paulovalves@outlook.com>
2021-03-03 11:44:53 -0300

    Better layout and a button to delete teh whole table

--- src/components/Table/NewTable.jsx
+++ src/components/Table/NewTable.jsx
@@ -2,26 +2,59 @@ import { useContext } from 'react';
 import styled from 'styled-components';
 import { DataContext } from '../../data/DataContext';
 import Food from './Food';
+import Button from '../Form/Button';
 
 const NewTable = () => {
-  const { toCalculateList } = useContext(DataContext);
-  return (
-    <Table>
-      {toCalculateList.length > 0 ? (
-        toCalculateList.map((food) => <Food key={food.id} food={food} />)
-      ) : (
+  const { toCalculateList, deleteTable } = useContext(DataContext);
+
+  if (toCalculateList.length > 0) {
+    return (
+      <TableForm>
+        <Table>
+          {toCalculateList.map((food) => (
+            <Food key={food.id} food={food} />
+          ))}
+        </Table>
+        <div className='btn'>
+          <Button buttonName='Limpar' onSubmit={deleteTable} />
+        </div>
+      </TableForm>
+    );
+  } else {
+    return (
+      <Table>
         <h3>Nenhum alimento foi adicionado à receita</h3>
-      )}
-    </Table>
-  );
+      </Table>
+    );
+  }
 };
+
+const TableForm = styled.div`
+  max-width: 50%;
+  margin: auto;
+
+  .btn {
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  Button {
+    background-color: #dd2222;
+    margin-left: auto;
+  }
+
+  Button:hover {
+    background-color: #aa4444;
+  }
+`;
 
 const Table = styled.div`
   display: block;
-  max-width: 50%;
-  margin: auto;
-  text-align: left;
   cursor: pointer;
+  max-width: 100%;
+  margin: auto;
+  margin-bottom: 10px;
+  text-align: left;
 
   h3 {
     text-align: center;
